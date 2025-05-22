2e877d63fb43461b430cedd1788f6d7eeba94359
theotrosman <theotrosman@gmail.com>
2025-05-21 23:38:02 -0300

    Upload proyecto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerProyecto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Escape()
+        {
+            return View();
+        }
+
+        public IActionResult Tutorial()
+        {
+            return View();
+        }
+        public IActionResult Room1()
+        {
+            return View();
+        }
+        public IActionResult GameStart()
+        {
+            return View();
+        }
+        public IActionResult Room2()
+        {
+            return View();
+        }
+        public IActionResult Room3()
+        {
+            return View();
+        }
+    }
+}
